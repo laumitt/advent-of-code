81d776da985729a90cf19354a83ed37ce93d95ca
Laurel Rodriguez Mitton <lmitton@olin.edu>
2023-12-01 01:11:27 -0500

    Completed 2023 Day 1

--- 2023/DAY 01/day01pt1.py
+++ 2023/DAY 01/day01pt1.py
@@ -1,15 +1,29 @@
 day = '01'
-test = 1
+test = 0
 if test == 1:
-    with open('day'+ day + 'input_test.txt') as f:
-        lines = f.readlines()
-elif test == 2:
-    with open('day'+ day + 'input_test2.txt') as f:
+    with open('DAY 01\day01input_test.txt') as f:
         lines = f.readlines()
 else:
-    with open('day'+ day + 'input.txt') as f:
+    with open('DAY 01\day01input.txt') as f:
         lines = f.readlines()
 input = []
 for a in range(len(lines)):
     input.append(lines[a].strip())
 print(input)
+
+numbers = []
+for calnum in input:
+    # print(calnum)
+    digits_this_line = []
+    for letter in calnum:
+        # print(letter)
+        if letter.isnumeric():
+            digits_this_line.append(letter)
+    numbers.append(digits_this_line[0]+digits_this_line[-1])
+
+total = 0
+for num in numbers:
+    print(num)
+    total += int(num)
+
+print(total)
--- 2023/DAY 01/day01pt2.py
+++ 2023/DAY 01/day01pt2.py
@@ -1,15 +1,45 @@
 day = '01'
-test = 1
+test = 0
 if test == 1:
-    with open('day'+ day + 'input_test.txt') as f:
+    with open('DAY 01\day01input_test.txt') as f:
         lines = f.readlines()
 elif test == 2:
-    with open('day'+ day + 'input_test2.txt') as f:
+    with open('DAY 01\day01input_test2.txt') as f:
         lines = f.readlines()
 else:
-    with open('day'+ day + 'input.txt') as f:
+    with open('DAY 01\day01input.txt') as f:
         lines = f.readlines()
 input = []
 for a in range(len(lines)):
     input.append(lines[a].strip())
 print(input)
+
+numberwords = {"one": 1, "two": 2, "three": 3, "four": 4, "five": 5,
+               "six": 6, "seven": 7, "eight": 8, "nine": 9,
+               "1": 1, "2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9}
+numbers = []
+
+def sortfunc(list):
+    return list['index']
+
+for calnum in input:
+    print(calnum)
+    digits_list = []
+    for num in numberwords:
+        if num in calnum:
+            digits_list.append({"digit": numberwords[num], "index": calnum.index(num)})
+            digits_list.append({"digit": numberwords[num], "index": calnum.rindex(num)})
+            print("num", num)
+            print("indexed", calnum[calnum.index(num)])
+            print("rindexed", calnum[calnum.rindex(num)])
+            print("dict", numberwords[num])
+    digits_list.sort(key=sortfunc)
+    print(digits_list)
+    numbers.append(str(digits_list[0]['digit'])+str(digits_list[-1]['digit']))
+
+total = 0
+for num in numbers:
+    print(num)
+    total += int(num)
+
+print(total)
